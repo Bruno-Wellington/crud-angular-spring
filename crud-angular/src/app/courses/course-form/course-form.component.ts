c4d4df0fd608bd66b0a50de5d6de3a1faa9f1fad
Bruno Carvalho <brunocarvalho.desenv@gmail.com>
2024-04-27 22:07:32 -0300

    Metodo para salvar um curso finalizado

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,6 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from '../services/courses.service';
@@ -11,12 +12,13 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CourseFormComponent implements OnInit {
 
-  form: FormGroup;
+  form: UntypedFormGroup;
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(private FormBuilder: UntypedFormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar) {
-    this.form = this.formBuilder.group({
+    private snackBar: MatSnackBar,
+  private location: Location) {
+    this.form = this.FormBuilder.group({
       name: [null],
       category: [null]
     })
@@ -26,16 +28,21 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit(){
     this.service.save(this.form.value).subscribe({
-      next:(result) => console.log(result),
+      next:(result) => this.onSuccess(),
       error:() =>{this.onError()},
     });
   }
 
   onCancel(){
-    console.log("Teste ok");
+    this.location.back();
+  }
+
+  private onSuccess(){
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 1500 });
+    this.onCancel();
   }
 
   private onError(){
-    this.snackBar.open('Erro ao salvar curso', '', { duration: 1500 });
+    this.snackBar.open('Erro ao salvar curso!', '', { duration: 1500 });
   }
 }
--- crud-spring/src/main/java/com/bruno/controller/CourseController.java
+++ crud-spring/src/main/java/com/bruno/controller/CourseController.java
@@ -2,8 +2,13 @@ package com.bruno.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bruno.model.Course;
@@ -28,4 +33,18 @@ public class CourseController {
     public List<Course> list(){
         return courseRepository.findAll();
     }
+
+    //@RequestMapping(method = RequestMethod.POST) ou
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+        //System.out.println(course.getName());
+        return courseRepository.save(course);
+    }
+    //ou
+    /* public ResponseEntity<Course> create(@RequestBody Course course){
+        //System.out.println(course.getName());
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(courseRepository.save(course));
+    }*/
 }
