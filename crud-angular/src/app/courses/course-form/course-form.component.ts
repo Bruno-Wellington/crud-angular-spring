3cf2dd6d1d611f3d358f8a380694ba92d75c6f32
Bruno Carvalho <brunocarvalho.desenv@gmail.com>
2024-04-29 21:20:06 -0300

    Refatorando codigo v1

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from '../services/courses.service';
@@ -12,22 +12,25 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CourseFormComponent implements OnInit {
 
-  form: UntypedFormGroup;
+  form = this.FormBuilder.group({
+    name: [''],
+    category: ['']
+  });
 
-  constructor(private FormBuilder: UntypedFormBuilder,
+  constructor(private FormBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
   private location: Location) {
-    this.form = this.FormBuilder.group({
-      name: [null],
-      category: [null]
-    })
+    //this.form
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
 
   onSubmit(){
-    this.service.save(this.form.value).subscribe({
+    this.service.save(this.form.value)
+    .subscribe({
       next:(result) => this.onSuccess(),
       error:() =>{this.onError()},
     });
