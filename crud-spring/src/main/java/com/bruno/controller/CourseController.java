a52a4a252c3d456a30e941d2d6a88b7757f50333
Bruno Carvalho <brunocarvalho.desenv@gmail.com>
2024-05-02 21:32:39 -0300

    Criado o metodo para buscar um curso pelo ID

--- crud-spring/src/main/java/com/bruno/controller/CourseController.java
+++ crud-spring/src/main/java/com/bruno/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,13 @@ public class CourseController {
     @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}") //Busca na Url o id
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+            .map(record -> ResponseEntity.ok().body(record))
+            .orElse(ResponseEntity.notFound().build());
     }
 
     //@RequestMapping(method = RequestMethod.POST) ou
