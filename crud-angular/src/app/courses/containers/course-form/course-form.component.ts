36a6b2440f2c305ca330b22530020b500979e6b6
Bruno Carvalho <brunocarvalho.desenv@gmail.com>
2024-05-06 20:35:28 -0300

    Levando infromações do curso para o form ao clicar no botão de editar

--- crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -2,7 +2,9 @@ import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 
+import { Course } from '../../model/course';
 import { CoursesService } from '../../services/courses.service';
 
 @Component({
@@ -13,6 +15,7 @@ import { CoursesService } from '../../services/courses.service';
 export class CourseFormComponent implements OnInit {
 
   form = this.FormBuilder.group({
+    _id: [''],
     name: [''],
     category: ['']
   });
@@ -20,11 +23,19 @@ export class CourseFormComponent implements OnInit {
   constructor(private FormBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
-  private location: Location) {
-  
+    private location: Location,
+    private route: ActivatedRoute) {
+
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    const course: Course = this.route.snapshot.data['course'];
+    this.form.setValue({
+      _id: course._id,
+      name: course.name,
+      category: course.category,
+    });
+  }
 
   onSubmit(){
     this.service.save(this.form.value)
