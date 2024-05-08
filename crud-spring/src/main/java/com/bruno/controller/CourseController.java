74c75b1b85e26340ffc5152934d25de5e2a4dedc
Bruno Carvalho <brunocarvalho.desenv@gmail.com>
2024-05-07 21:24:48 -0300

    Metodo do back-end para atualizar um curso finalizado - Fim

--- crud-spring/src/main/java/com/bruno/controller/CourseController.java
+++ crud-spring/src/main/java/com/bruno/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,7 +39,7 @@ public class CourseController {
     @GetMapping("/{id}") //Busca na Url o id
     public ResponseEntity<Course> findById(@PathVariable Long id) {
         return courseRepository.findById(id)
-            .map(record -> ResponseEntity.ok().body(record))
+            .map(recordFound -> ResponseEntity.ok().body(recordFound))
             .orElse(ResponseEntity.notFound().build());
     }
 
@@ -55,4 +56,16 @@ public class CourseController {
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(courseRepository.save(course));
     }*/
+
+    @PutMapping("/{id}")//Busca na Url o id
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course){
+        return courseRepository.findById(id)
+            .map(recordFound -> {
+                recordFound.setName(course.getName());
+                recordFound.setCategory(course.getCategory());
+                Course updated = courseRepository.save(recordFound);
+                return ResponseEntity.ok().body(updated);
+            })
+            .orElse(ResponseEntity.notFound().build());//tratamento caso nao ache o id
+    }
 }
